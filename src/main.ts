51763f7954e396d0021bbf5e364e5e089a6a9257
tanapa-p <44843902+tanapa-p@users.noreply.github.com>
2024-07-04 15:53:47 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
